27a258b8773bd45d7fd5edb2b656e95289d786d2
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-20 19:10:01 +0300

    del

---
+++ split_strings.py
@@ -0,0 +1,10 @@
+def solution(s):
+    result = []
+    if len(s) % 2 != 0:
+        s += '_'
+    for i in range(0, len(s), 2):
+        result.append(s[i:i + 2])
+    return result
+
+
+print(solution('abcde'))
