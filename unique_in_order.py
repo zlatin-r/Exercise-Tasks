8e01b68d3f58bfd3dbf3e4b7c58584b96e39568e
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-21 16:15:32 +0200

    Solution

---
+++ unique_in_order.py
@@ -0,0 +1,24 @@
+def unique_in_order(iterable):
+    result = []
+    prev = None
+
+    for item in iterable:
+        if item != prev:
+            result.append(item)
+            prev = item
+
+    return result
+
+
+# def unique_in_order(iterable):
+#     res = []
+#     for item in iterable:
+#         if len(res) == 0 or item != res[-1]:
+#             res.append(item)
+#     return res
+
+
+print(unique_in_order('AAAABBBCCDAABBB'))
+print(unique_in_order('ABBCcAD'))
+print(unique_in_order([1, 2, 2, 3, 3]))
+print(unique_in_order((1, 2, 2, 3, 3)))
