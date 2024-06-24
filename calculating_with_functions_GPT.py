bee6f824159428f6ce5b89ac2a7595dfeae37863
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-24 14:29:24 +0200

    Solution

---
+++ calculating_with_functions_GPT.py
@@ -0,0 +1,61 @@
+def zero(func=None):
+    return 0 if func is None else func(0)
+
+
+def one(func=None):
+    return 1 if func is None else func(1)
+
+
+def two(func=None):
+    return 2 if func is None else func(2)
+
+
+def three(func=None):
+    return 3 if func is None else func(3)
+
+
+def four(func=None):
+    return 4 if func is None else func(4)
+
+
+def five(func=None):
+    return 5 if func is None else func(5)
+
+
+def six(func=None):
+    return 6 if func is None else func(6)
+
+
+def seven(func=None):
+    return 7 if func is None else func(7)
+
+
+def eight(func=None):
+    return 8 if func is None else func(8)
+
+
+def nine(func=None):
+    return 9 if func is None else func(9)
+
+
+def plus(y):
+    return lambda x: x + y
+
+
+def minus(y):
+    return lambda x: x - y
+
+
+def times(y):
+    return lambda x: x * y
+
+
+def divided_by(y):
+    return lambda x: x // y  # Use integer division
+
+
+# Examples
+print(seven(times(five())))  # must return 35
+print(four(plus(nine())))  # must return 13
+print(eight(minus(three())))  # must return 5
+print(six(divided_by(two())))  # must return 3
