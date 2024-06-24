3042dd6d02d8d269bd97a739a157eb075e443300
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-24 14:22:48 +0200

    Solution

---
+++ new_task.py
@@ -0,0 +1,15 @@
+def zero(): pass #your code here
+def one(): pass #your code here
+def two(): pass #your code here
+def three(): pass #your code here
+def four(): pass #your code here
+def five(): pass #your code here
+def six(): pass #your code here
+def seven(): pass #your code here
+def eight(): pass #your code here
+def nine(): pass #your code here
+
+def plus(): pass #your code here
+def minus(): pass #your code here
+def times(): pass #your code here
+def divided_by(): pass #your code here
