92ff20fd040ac4bc160fe1ed3906537e170fd8f2
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-01 16:33:23 +0200

    Solution

--- second_lowest_grades.py
+++ second_lowest_grades.py
@@ -1,28 +1,26 @@
-if __name__ == '__main__':
+data = []
 
-    data = []
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
 
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
+    name_grade = []
 
-        name_grade = []
+    name_grade.append(name)
+    name_grade.append(score)
 
-        name_grade.append(name)
-        name_grade.append(score)
+    data.append(name_grade)
 
-        data.append(name_grade)
+sorted_data = sorted(data, key=lambda x: x[1])
 
-    sorted_data = sorted(data, key=lambda x: x[1])
+min_grade = min(grade[1] for grade in sorted_data)
 
-    min_grade = min(grade[1] for grade in sorted_data)
+removed_min_grades = [sublist for sublist in sorted_data if sublist[1] != min_grade]
 
-    removed_min_grades = [sublist for sublist in sorted_data if sublist[1] != min_grade]
+min_grade = min(grade[1] for grade in removed_min_grades)
 
-    min_grade = min(grade[1] for grade in removed_min_grades)
+filtered_list = [sublist for sublist in removed_min_grades if sublist[1] == min_grade]
 
-    filtered_list = [sublist for sublist in removed_min_grades if sublist[1] == min_grade]
+result = '\n'.join(name[0] for name in sorted(filtered_list))
 
-    result = '\n'.join(name[0] for name in sorted(filtered_list))
-
-    print(result)
+print(result)
