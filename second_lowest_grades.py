2f763948526b723f9b99089755537e641aedae97
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-01 16:32:24 +0200

    Solution

---
+++ second_lowest_grades.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+
+    data = []
+
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+
+        name_grade = []
+
+        name_grade.append(name)
+        name_grade.append(score)
+
+        data.append(name_grade)
+
+    sorted_data = sorted(data, key=lambda x: x[1])
+
+    min_grade = min(grade[1] for grade in sorted_data)
+
+    removed_min_grades = [sublist for sublist in sorted_data if sublist[1] != min_grade]
+
+    min_grade = min(grade[1] for grade in removed_min_grades)
+
+    filtered_list = [sublist for sublist in removed_min_grades if sublist[1] == min_grade]
+
+    result = '\n'.join(name[0] for name in sorted(filtered_list))
+
+    print(result)
