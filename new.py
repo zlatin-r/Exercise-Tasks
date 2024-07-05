449668cafda57d16ecfcb0edb6c1b69439f8b12a
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-05 14:16:09 +0200

    Solution

--- new.py
+++
@@ -1,8 +0,0 @@
-original_string = input("Enter the original string: ")
-substring = input("Enter the substring: ")
-
-# Count the occurrences of the substring in the original string
-count = original_string.count(substring)
-
-# Print the result
-print(count)
