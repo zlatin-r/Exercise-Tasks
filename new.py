9ff977e5d0eeeff544d1f9049271dd20e66aa125
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-05 13:33:43 +0200

    Solution

---
+++ new.py
@@ -0,0 +1,9 @@
+first_name = input()
+last_name = input()
+
+
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
+
+
+print(print_full_name(first_name, last_name))
