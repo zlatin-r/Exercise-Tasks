b9fa571c054b4d68c64733d78168064ffcd7ec35
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-13 16:48:45 +0200

    Solution

--- collections_namedtuple.py
+++ collections_namedtuple.py
@@ -1,11 +1,17 @@
 from collections import namedtuple
 
 total_students = int(input())
-fields = input().split()
-student = namedtuple('student', 'id, marks, name, class')
-data = []
+Student = namedtuple("Student", input().split())
 sum_marks = 0
 
 for i in range(total_students):
-    s = student(input().split())
-    sum_marks += s.marks
+    data = input().split()
+
+    s = Student(data[0], data[1], data[2], data[3])
+
+    sum_marks += int(s.MARKS)
+
+print(f"{sum_marks / total_students:.2f}")
+
+# students_num, total, student = int(input()), 0, namedtuple('student', ",".join(input().split()))
+# print(sum(int(student(*[a for a in input().split()]).MARKS) for a in range(students_num)) / students_num)
