9fe851cebcb5ff5b3274bed501de151be1e3e84f
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-22 14:28:02 +0200

    Solution

---
+++ set_intersection.py
@@ -0,0 +1,7 @@
+english_subs = int(input())
+en_students = set(map(int, input().split()))
+french_subs = int(input())
+fr_students = set(map(int, input().split()))
+
+print(len(en_students.intersection(fr_students)))
+
