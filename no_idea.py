00fa439f5a3b23d8e9d77429acf9a622f30d7a82
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-22 14:01:00 +0200

    Solution

---
+++ no_idea.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+
+happiness = 0
+
+for i in arr:
+    if i in a:
+        happiness += 1
+    if i in b:
+        happiness -= 1
+
+print(happiness)
