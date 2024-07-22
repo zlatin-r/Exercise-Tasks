ce41d5f56e7d4ebdb5aaab7b9ea46782d572b1a2
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-22 14:26:42 +0200

    Solution

---
+++ set_union.py
@@ -0,0 +1,6 @@
+english_subs = int(input())
+en_students = set(map(int, input().split()))
+french_subs = int(input())
+fr_students = set(map(int, input().split()))
+
+print(len(en_students.union(fr_students)))
