093c2fdd65379dba9fdfd7c360c16ac925469e1d
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-14 20:18:49 +0200

    Solution

--- calendar_module.py
+++ calendar_module.py
@@ -1,14 +1,4 @@
 import calendar
 
 month, day, year = map(int, input().split())
-week = {
-    1: "Monday",
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday",
-    7: "Sunday"
-}
-
-print(week[calendar.weekday(year, month, day)])
+print(calendar.day_name[calendar.weekday(year, month, day)])
