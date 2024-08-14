be7f88206475f7c2acd8873d7e6db40d4db0134a
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-14 20:21:59 +0200

    Solution

--- calendar_module.py
+++ calendar_module.py
@@ -2,4 +2,4 @@ import calendar
 
 month, day, year = map(int, input().split())
 
-print(calendar.day_name[calendar.weekday(year, month, day)])
+print(calendar.day_name[calendar.weekday(year, month, day)].upper())
